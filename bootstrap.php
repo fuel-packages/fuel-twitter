3b3a5272c24490dfb791b7071e0667ab6835a3ba
Dan Horrigan <dan@dhorrigan.com>
2011-08-31 00:10:24 -0400

    Had bad paths in the bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ Autoloader::add_core_namespace('Twitter');
 Autoloader::add_classes(array(
 	'Twitter\\Twitter'                 => __DIR__.'/classes/twitter.php',
 	'Twitter\\TwitterException'        => __DIR__.'/classes/twitter.php',
-	'Twitter\\Twitter_Connection'      => __DIR__.'/classes/connection.php',
-	'Twitter\\Twitter_Oauth'           => __DIR__.'/classes/oauth.php',
-	'Twitter\\Twitter_Oauth_Response'  => __DIR__.'/classes/oauth/response.php',
+	'Twitter\\Twitter_Connection'      => __DIR__.'/classes/twitter/connection.php',
+	'Twitter\\Twitter_Oauth'           => __DIR__.'/classes/twitter/oauth.php',
+	'Twitter\\Twitter_Oauth_Response'  => __DIR__.'/classes/twitter/oauth/response.php',
 ));
